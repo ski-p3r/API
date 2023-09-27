1c6d97a7100550e13b29ccd8c61600b65bc98ad7
Mubarek Shikur <manmuba90@gmail.com>
2023-09-27 19:23:43 +0300

    Cart and Cart Schema created

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,21 @@
+import uuid
 from django.db import models
+from shop.models import Product
 
 # Create your models here.
+
+class Cart(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.id
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.PositiveIntegerField(default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.cart.id
